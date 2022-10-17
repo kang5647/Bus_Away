1cc7f8a5c565948ea4e7ac938e4af06652276e8d
Wei Kang <76279908+kang5647@users.noreply.github.com>
2022-10-17 23:49:46 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/bus_away_firestore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bus_away_firestore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:bus_away_firestore/homepage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bus Route Search',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FirebaseSearchScreen(),
+    );
+  }
+}
+
+class FirebaseSearchScreen extends StatefulWidget {
+  const FirebaseSearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FirebaseSearchScreen> createState() => _FirebaseSearchScreenState();
+}
+
+class _FirebaseSearchScreenState extends State<FirebaseSearchScreen> {
+  List searchResult = [];
+
+  void searchFromFirebase(String query) async {
+    final result = await FirebaseFirestore.instance
+        .collection('busRoutes')
+        .where('ServiceNo', isEqualTo: query)
+        .get();
+
+    setState(() {
+      searchResult = result.docs.map((e) => e.data()).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Firebase Search"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Enter Bus Service No.",
+              ),
+              onChanged: (query) {
+                searchFromFirebase(query);
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: searchResult.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(searchResult[index]['ServiceNo']),
+                  subtitle: Text(searchResult[index]['BusStopCode']),
+                  trailing: Text(searchResult[index]['Direction'].toString()),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
