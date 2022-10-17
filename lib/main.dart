b5872d5aec9aa32867d5470705345cdd8b0c4a1b
Wei Kang <76279908+kang5647@users.noreply.github.com>
2022-10-17 23:56:11 +0800

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bus_away_firestore/homepage.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
