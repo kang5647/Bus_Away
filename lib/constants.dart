e5ecaf182570f9811360f2206529d668796718e3
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-10-28 17:21:21 +0800

    gmail (natural reserve path)

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyACTnTxalaLcULQEuR5r5dPkDi8SxcBNuw";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
